9f06161b71331ff689f4871986314351143e658e
Rohit99332 <rohitshelar993322@gmail.com>
2025-06-28 12:19:04 +0000

    var,datatypes

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="rohit99332@gmail.com"
+var accountPassword="1234"
+accountCity="pune"
+
+accountEmail="gc@gc.com"
+accountPassword="343234"
+accountCity="malegaon"
+
+//accountId=2
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])

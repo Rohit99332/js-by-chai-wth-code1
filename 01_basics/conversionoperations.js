ffdfd5a03d4c205c5dfa29ed676ca954d6ab16b1
Rohit99332 <rohitshelar993322@gmail.com>
2025-06-28 13:04:20 +0000

    conversion

--- 01_basics/conversionoperations.js
+++ 01_basics/conversionoperations.js
@@ -1,4 +1,4 @@
-let score="33abc"
+/*let score="33abc"
 console.log(typeof score);
 console.log(typeof(score));
 
@@ -11,4 +11,9 @@ let someNumber=33
 
 let stringNumber = String(someNumber)
 console.log(stringNumber);
-console.log(typeof stringNumber);
+console.log(typeof stringNumber);*/
+
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)

bf5586bd780355919e88bfd61fc6e3a5dd6a99a4
Rohit99332 <rohitshelar993322@gmail.com>
2025-06-29 06:50:12 +0000

    memory

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -6,3 +6,42 @@
 //undefined
 //symbol=>unique
 //object
+
+
+
+
+//primitive datatypes
+//seven types: string , number ,boolean , null , undefine , symbol , BigInt
+
+
+//reference or non primitive datatype
+
+//Araay,Objects,Functions 
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++
+
+
+//memory
+
+
+//Stack (primitive),Heap (Non-primitive)
+
+
+let myName="rohit"
+let anotherName="dada"
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+
+let userTwo=userOne
+
+userTwo.email="rohit@gmail.com "
+
+console.log(userOne.email);
+console.log(userTwo.email);
